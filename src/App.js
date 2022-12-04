e20e4b396a4b59a3f4892f9c5d00559790e9e075
Cindy Luo <cindyluo2015@gmail.com>
2022-12-04 02:06:53 -0500

    archive current website (#3)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React from 'react';
 
-import Header from './components/Header';
-import Home from './pages/Home';
-import About from './pages/About';
-import Experience from './pages/Experience';
-import Projects from './pages/Projects';
-import Photography from './pages/Photography';
-
 const App = () => {
   return (
-    <> 
-      <Header/>
-      <Home />
-      <About />
-      <Experience />
-      <Projects />
-      <Photography />
+    <>
+      CURRENTLY UNDER CONSTRUCTIONS :D
     </>
   );
 }
